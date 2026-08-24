bc54ec547d826e8e8cec653cac937116da08f8d3
Animatic_Almanac <67105502+CambellJSmith@users.noreply.github.com>
2026-08-24 17:45:26 +0100

    Add aquarium supply data

---
+++ js/data/supply_items.js
@@ -0,0 +1,23 @@
+export const SUPPLY_ITEMS = Object.freeze([
+    Object.freeze({
+        id: "food",
+        name: "fish_food",
+        price: 6,
+        icon: "●",
+        description: "one_portion_feeds_every_fish_in_the_tank_and_reduces_their_hunger."
+    }),
+    Object.freeze({
+        id: "sponge",
+        name: "cleaning_sponge",
+        price: 10,
+        icon: "▦",
+        description: "one_sponge_scrubs_the_tank_clean_and_resets_the_dirt_level."
+    }),
+    Object.freeze({
+        id: "medicine",
+        name: "fish_medicine",
+        price: 14,
+        icon: "+",
+        description: "one_dose_treats_the_fish_with_the_lowest_health_in_your_tank."
+    })
+]);
